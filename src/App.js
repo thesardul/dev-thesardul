8f66a8591ed730b717d8d5740e6283a401e3d001
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-08-12 00:50:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+      <h3>Welcome to the corner of my Internet of things...</h3>
+    </div>
+  );
+}
+
+export default App;
